bd5caf361a4e4b444c686e92a385ce3950bcd6b6
AllisonSilvaDev <alvesallison42@gmail.com>
2025-04-20 16:55:06 -0300

    20-04-25

--- back/api/models.py
+++ back/api/models.py
@@ -4,5 +4,6 @@ from django.db import models
 class Produto(models.Model):
     nome = models.CharField(max_length=255)
     preco = models.IntegerField()
+    imagem = models.CharField(max_length=255)
 
 
