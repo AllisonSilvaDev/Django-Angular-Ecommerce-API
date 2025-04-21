1a6fb0c9a791fb965fe4841131933f0b988ccab7
AllisonSilvaDev <alvesallison42@gmail.com>
2025-04-21 15:26:37 -0300

    adjustments

--- front/src/app/components/card-list/card-list.component.ts
+++ front/src/app/components/card-list/card-list.component.ts
@@ -1,25 +1,31 @@
-import { Component} from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card-list',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './card-list.component.html',
-  styleUrl: './card-list.component.css'
+  styleUrls: ['./card-list.component.css']
 })
+export class CardListComponent implements OnInit {
 
-export class CardListComponent implements OnInit{
-  dados: any;
+  dados: any[] = [];
+
+  @Input() inicio!: number;
+  @Input() limite!: number;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get('http://127.0.0.1:8000/api/produtos/').subscribe(
+    this.http.get<any[]>('http://127.0.0.1:8000/api/produtos/').subscribe(
       (res) => {
         this.dados = res;
         console.log(this.dados);
+      },
+      (error) => {
+        console.error('Erro ao buscar dados:', error);
       }
     );
   }
